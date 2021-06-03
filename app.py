e1e23aab6aeaf13bb9df8392734fd3b8d3bedac8
Abhi <mailtoabhijithofficial@gmail.com>
2021-06-03 14:56:36 +0530

    'initial'

--- app.py
+++ app.py
@@ -29,29 +29,33 @@ def index():
 def respond():
     try:
         update = telegram.Update.de_json(request.get_json(force=True), bot)
-        if(update is not None):
+        if(update is not None and(update.message is not None ) ):
             session["info"] = {
 
                 "username" : update.message.chat.first_name,
                 "text" : update.message.text,
                 "chat_id" : update.message.chat.id,
-                "msg_id" : update.message.message_id,
+                "msg_id" : update.message.message_id
+        
 
                 }
             print("msg id",session["info"]["msg_id"]," chatid ",session["info"]["chat_id"], " " , session["info"]["username"])
             if(session["info"]["text"] == "/start" ):
                 db.setstate(session["info"]["chat_id"],"start")
+                session["counter"]=0
 
             if(db.getstate(session["info"]["chat_id"]) == "start"):
                 
                 welcometext = "Hi, {username}. Welcome to VaxBot. You can use this bot to check if Covid-19 vaccination is available in your area. Please Choose from the following options ".format(username=session["info"]["username"])
-                custom_keyboard = [['Search By Pincode'],['Search By District']]
+                custom_keyboard = [['Search By Pincode'],['Search By State']]
                 reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard,one_time_keyboard=True)
                 y = bot.sendMessage(chat_id=session["info"]["chat_id"],text=welcometext,reply_to_message_id=session["info"]["msg_id"],reply_markup=reply_markup)
+                session["counter"]=1
                 if y:
                     db.setstate(session["info"]["chat_id"],"check")
 
             elif(session["info"]["text"] == "Search By Pincode" and db.getstate(session["info"]["chat_id"]) == "check"):
+
                 
                 #change statecounter to pin
                 pintext = "Enter your pincode and the date on which you would like to get vaccinated.\nThe format should be: pincode <space> DD-MM-YYYY \nEg: 110110 31-03-2021 "
@@ -61,6 +65,70 @@ def respond():
                 if x:
                     db.setstate(session["info"]["chat_id"] ,"pin")
 
+            elif(session["info"]["text"] == "Search By State" and db.getstate(session["info"]["chat_id"]) == "check"):
+                statetext = "Enter the name of your state"
+                s = bot.sendMessage(chat_id=session["info"]["chat_id"],text=statetext,reply_to_message_id=session["info"]["msg_id"])
+                if s:
+                    db.setstate(session["info"]["chat_id"] ,"state")
+
+            
+            elif((session["info"]["text"].isalpha() == True) and (db.getstate(session["info"]["chat_id"])  == "state")):
+                stateid = db.findstateid(session["info"]["text"])
+                
+                if(stateid == False):
+                    bot.sendMessage(chat_id=session["info"]["chat_id"],text="Couldn't find state. There could be a problem with spelling. /start again")
+                #send messge to enter district and set state to district 
+                else:
+                    reptext = "Enter the name of your district in {statename}".format(statename = session["info"]["text"])
+                    db.setstateid(session["info"]["chat_id"] ,stateid)
+                    #print(session["stateid"])
+                    n = bot.sendMessage(chat_id=session["info"]["chat_id"],text=reptext,reply_to_message_id=session["info"]["msg_id"])
+                    if n:
+                        db.setstate(session["info"]["chat_id"] ,"district")
+
+            
+
+            elif((session["info"]["text"].isalpha() == True) and (db.getstate(session["info"]["chat_id"])  == "district")):
+                stval = db.getstateid(session["info"]["chat_id"])
+                if(stval!=None):
+                    val = db.getdistrict(stval,session["info"]["text"].lower())
+                    print(val)
+                    if(val == False):
+                        bot.sendMessage(chat_id=session["info"]["chat_id"],text="Wrong name. Must be a problem with spelling. /start again")
+
+                    else:
+                        #ivde session["distid"] =val
+                        db.setdistrictid(session["info"]["chat_id"],val)
+                        datt = "Enter date on which you would like to get vaccinated.\nFormat DD-MM-YYYY\nEg: 02-02-2021"
+                        k = bot.sendMessage(chat_id=session["info"]["chat_id"],text=datt)
+                        if k:
+                            db.setstate(session["info"]["chat_id"] ,"date")
+                    
+
+
+            
+            elif((session["info"]["text"].isalpha() == False) and (db.getstate(session["info"]["chat_id"])  == "date")):
+            
+                distid_d = db.getdistid(session["info"]["chat_id"])
+                if(distid_d is not None):
+                    resp1 = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={distid}&date={date}".format(distid=distid_d,date=session["info"]["text"]))
+                    j1 = resp1.json()
+                    if(j1 is not None):
+                        for j in j1["sessions"]:
+                            h1_name = j["name"]
+                            h1_addr = j["address"] 
+                            h1_cap1 = j["available_capacity_dose1"]  
+                            h1_cap2 = j["available_capacity_dose2"]
+                            total1 = j["available_capacity"]
+                            det1 =   "{hos_name} \n{hos_adr} \nDose one Available:{hos_cap1} \nDose two Available:{hos_cap2}\nTotal Dose:{total1}".format(hos_name=h1_name,hos_adr = h1_addr,hos_cap1=h1_cap1, hos_cap2 = h1_cap2,total1=total1)
+                            bot.sendMessage(chat_id=session["info"]["chat_id"],text=det1)
+                        
+                        continuetext1 = "would you like to coninue"
+                        custom_keyboard1 = [['Yes'],['No']]
+                        reply_markup1 = telegram.ReplyKeyboardMarkup(custom_keyboard1,one_time_keyboard=True)
+                        zss = bot.sendMessage(chat_id=session["info"]["chat_id"],text=continuetext1,reply_to_message_id=session["info"]["msg_id"],reply_markup=reply_markup1)
+                        if zss:
+                            db.setstate(session["info"]["chat_id"],"continue")
 
             
 
@@ -80,13 +148,14 @@ def respond():
                     hos_address = i["address"]
                     hos_cap1 = i['available_capacity_dose1']
                     hos_cap2 = i['available_capacity_dose2']
-                    details = "{hos_name} \n{hos_adr} \nDose one Available:{hos_cap1} \nDose two Available:{hos_cap2}".format(hos_name=hos_name,hos_adr = hos_address,hos_cap1=hos_cap1, hos_cap2 = hos_cap2)
+                    total = i['available_capacity']
+                    details = "{hos_name} \n{hos_adr} \nDose one Available:{hos_cap1} \nDose two Available:{hos_cap2}\n Total Doses: {total}".format(hos_name=hos_name,hos_adr = hos_address,hos_cap1=hos_cap1, hos_cap2 = hos_cap2,total=total)
                     bot.sendMessage(chat_id=session["info"]["chat_id"],text=details)
 
-                continuetext = "would you like to coninue"
-                custom_keyboard = [['Yes'],['No']]
-                reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard,one_time_keyboard=True)
-                z = bot.sendMessage(chat_id=session["info"]["chat_id"],text=continuetext,reply_to_message_id=session["info"]["msg_id"],reply_markup=reply_markup)
+                continuetext2 = "would you like to coninue"
+                custom_keyboard2 = [['Yes'],['No']]
+                reply_markup2 = telegram.ReplyKeyboardMarkup(custom_keyboard2,one_time_keyboard=True)
+                z = bot.sendMessage(chat_id=session["info"]["chat_id"],text=continuetext2,reply_to_message_id=session["info"]["msg_id"],reply_markup=reply_markup2)
                 if z:
                     db.setstate(session["info"]["chat_id"],"continue")
 
@@ -102,14 +171,16 @@ def respond():
             elif((session["info"]["text"] == "No") and (db.getstate(session["info"]["chat_id"])  == "continue")):
                 thankmessage="Thanks for using VaxBot. If you want to check again, just type in /start onto the textarea. :)"
                 bot.sendMessage(chat_id=session["info"]["chat_id"],text=thankmessage,reply_to_message_id=session["info"]["msg_id"])   
+            else:
+                bot.sendMessage(chat_id=session["info"]["chat_id"],text="Invalid Command. Please /start again.",reply_to_message_id=session["info"]["msg_id"]) 
     except:
-        print("I don't know")
+        print("Some error")
     return "done"
 
 
 @app.route("/setwebhook/",methods=["GET","POST"])
 def setwebhook():
-    url = "https://e97a43185b82.ngrok.io/"
+    url = "https://f9bf20fdfb7e.ngrok.io/"
     s = bot.setWebhook("{url}{key}".format(url=url,key=key))
   
     if s:
--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from flask_pymongo import pymongo
+import requests
 connectionurl = "mongodb+srv://test:test@cluster0.9xteb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 client  = pymongo.MongoClient(connectionurl)
 db = client.get_database('botbase')
@@ -15,6 +16,45 @@ def getstate(msgid):
     else:
         return x["state"]
 
+def setstateid(msgid,val):
+    x = point.find_one({"msgid":msgid})
+    if(x is not None):
+        point.update_one({"msgid":msgid} ,
+        {
+            '$set':{
+                'stateid':val
+
+            }
+        },upsert=False)
+        return True
+#setstateid(633111810,"17")
+def getstateid(msgid):
+    x= point.find_one({"msgid":msgid})
+    if x is not None:
+        return x["stateid"]
+    else:
+        return None
+#print(getstateid(633111810))
+def setdistrictid(msgid,val):
+    x = point.find_one({"msgid":msgid})
+    if(x is not None):
+        point.update_one({"msgid":msgid} ,
+        {
+            '$set':{
+                'distid':val
+
+            }
+        },upsert=False)
+        return True
+
+#setdistrictid(633111810,209)
+def getdistid(msgid):
+    x= point.find_one({"msgid":msgid})
+    if x is not None:
+        return x["distid"]
+    else:
+        return None
+# print(getdistid(633111810))
 def setstate(msgid,stateval):
     x = point.find_one({"msgid":msgid})
     if(x is not None):
@@ -26,160 +66,36 @@ def setstate(msgid,stateval):
             }
         },upsert=False)
         return True
+def findstateid(text):
+    x = st.find_one({"state_name":text.lower()})
+    if x is not None:
+        return x["state_id"]
+    else:
+        return False
 
-# statess=[
-#       {
-#          "state_id":1,
-#          "state_name":"Andaman and Nicobar Islands"
-#       },
-#       {
-#          "state_id":2,
-#          "state_name":"Andhra Pradesh"
-#       },
-#       {
-#          "state_id":3,
-#          "state_name":"Arunachal Pradesh"
-#       },
-#       {
-#          "state_id":4,
-#          "state_name":"Assam"
-#       },
-#       {
-#          "state_id":5,
-#          "state_name":"Bihar"
-#       },
-#       {
-#          "state_id":6,
-#          "state_name":"Chandigarh"
-#       },
-#       {
-#          "state_id":7,
-#          "state_name":"Chhattisgarh"
-#       },
-#       {
-#          "state_id":8,
-#          "state_name":"Dadra and Nagar Haveli"
-#       },
-#       {
-#          "state_id":37,
-#          "state_name":"Daman and Diu"
-#       },
-#       {
-#          "state_id":9,
-#          "state_name":"Delhi"
-#       },
-#       {
-#          "state_id":10,
-#          "state_name":"Goa"
-#       },
-#       {
-#          "state_id":11,
-#          "state_name":"Gujarat"
-#       },
-#       {
-#          "state_id":12,
-#          "state_name":"Haryana"
-#       },
-#       {
-#          "state_id":13,
-#          "state_name":"Himachal Pradesh"
-#       },
-#       {
-#          "state_id":14,
-#          "state_name":"Jammu and Kashmir"
-#       },
-#       {
-#          "state_id":15,
-#          "state_name":"Jharkhand"
-#       },
-#       {
-#          "state_id":16,
-#          "state_name":"Karnataka"
-#       },
-#       {
-#          "state_id":17,
-#          "state_name":"Kerala"
-#       },
-#       {
-#          "state_id":18,
-#          "state_name":"Ladakh"
-#       },
-#       {
-#          "state_id":19,
-#          "state_name":"Lakshadweep"
-#       },
-#       {
-#          "state_id":20,
-#          "state_name":"Madhya Pradesh"
-#       },
-#       {
-#          "state_id":21,
-#          "state_name":"Maharashtra"
-#       },
-#       {
-#          "state_id":22,
-#          "state_name":"Manipur"
-#       },
-#       {
-#          "state_id":23,
-#          "state_name":"Meghalaya"
-#       },
-#       {
-#          "state_id":24,
-#          "state_name":"Mizoram"
-#       },
-#       {
-#          "state_id":25,
-#          "state_name":"Nagaland"
-#       },
-#       {
-#          "state_id":26,
-#          "state_name":"Odisha"
-#       },
-#       {
-#          "state_id":27,
-#          "state_name":"Puducherry"
-#       },
-#       {
-#          "state_id":28,
-#          "state_name":"Punjab"
-#       },
-#       {
-#          "state_id":29,
-#          "state_name":"Rajasthan"
-#       },
-#       {
-#          "state_id":30,
-#          "state_name":"Sikkim"
-#       },
-#       {
-#          "state_id":31,
-#          "state_name":"Tamil Nadu"
-#       },
-#       {
-#          "state_id":32,
-#          "state_name":"Telangana"
-#       },
-#       {
-#          "state_id":33,
-#          "state_name":"Tripura"
-#       },
-#       {
-#          "state_id":34,
-#          "state_name":"Uttar Pradesh"
-#       },
-#       {
-#          "state_id":35,
-#          "state_name":"Uttarakhand"
-#       },
-#       {
-#          "state_id":36,
-#          "state_name":"West Bengal"
-#       }
-#    ]
 
-# for i in statess:
-#     print(i)
+def getdistrict(ids,dname):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    resp = requests.get("https://cdn-api.co-vin.in/api/v2/admin/location/districts/{ids}".format(ids=ids),headers = headers)
+    diction = resp.json()
+    li = diction["districts"]
+    if len(li) != 0:
+        for i in li:
+            if(i["district_name"].lower() == dname):
+                return i["district_id"]
+                
+    return False
+    
+
+
+# o = getdistrict("17","Kollam")
+# print(o)
+# print(o)
+
+# statess=[{'state_id': 1, 'state_name': 'andaman and nicobar islands'}, {'state_id': 2, 'state_name': 'andhra pradesh'}, {'state_id': 3, 'state_name': 'arunachal pradesh'}, {'state_id': 4, 'state_name': 'assam'}, {'state_id': 5, 'state_name': 'bihar'}, {'state_id': 6, 'state_name': 'chandigarh'}, {'state_id': 7, 'state_name': 'chhattisgarh'}, {'state_id': 8, 'state_name': 'dadra and nagar haveli'}, {'state_id': 37, 'state_name': 'daman and diu'}, {'state_id': 9, 'state_name': 'delhi'}, {'state_id': 10, 'state_name': 'goa'}, {'state_id': 11, 'state_name': 'gujarat'}, {'state_id': 12, 'state_name': 'haryana'}, {'state_id': 13, 'state_name': 'himachal pradesh'}, {'state_id': 14, 'state_name': 'jammu and kashmir'}, {'state_id': 15, 'state_name': 'jharkhand'}, {'state_id': 16, 'state_name': 'karnataka'}, {'state_id': 17, 'state_name': 'kerala'}, {'state_id': 18, 'state_name': 'ladakh'}, {'state_id': 19, 'state_name': 'lakshadweep'}, {'state_id': 20, 'state_name': 'madhya pradesh'}, {'state_id': 21, 'state_name': 'maharashtra'}, {'state_id': 22, 'state_name': 'manipur'}, {'state_id': 23, 'state_name': 'meghalaya'}, {'state_id': 24, 'state_name': 'mizoram'}, {'state_id': 25, 'state_name': 'nagaland'}, {'state_id': 26, 'state_name': 'odisha'}, {'state_id': 27, 'state_name': 'puducherry'}, {'state_id': 28, 'state_name': 'punjab'}, {'state_id': 29, 'state_name': 'rajasthan'}, {'state_id': 30, 'state_name': 'sikkim'}, {'state_id': 31, 'state_name': 'tamil nadu'}, {'state_id': 32, 'state_name': 'telangana'}, {'state_id': 33, 'state_name': 'tripura'}, {'state_id': 34, 'state_name': 'uttar pradesh'}, {'state_id': 35, 'state_name': 'uttarakhand'}, {'state_id': 36, 'state_name': 'west bengal'}]
+
+# # for i in statess:
+# #     print(i)
 
 # def gomap():
 #     for i in statess:
