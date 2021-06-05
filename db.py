cd7d19216c245c21706bd4863ff0ee1dc500aec0
Abhi <mailtoabhijithofficial@gmail.com>
2021-06-05 18:40:06 +0530

    'addingsome'

--- db.py
+++ db.py
@@ -75,7 +75,7 @@ def findstateid(text):
 
 
 def getdistrict(ids,dname):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36','referer':'https://vaxbotapp.herokuapp.com/'}
+    headers = {"Accept-Language": "hi_IN", 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     resp = requests.get("https://cdn-api.co-vin.in/api/v2/admin/location/districts/{ids}".format(ids=ids),headers=headers)
     print(resp.status_code)
     diction = resp.json()
